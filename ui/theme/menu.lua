e241a43873e07d06cae5f830e02baa96a16dfc06
MOIS3Y <stepan@zhukovsky.me>
2023-11-01 11:27:49 +0900

    Modify: menu theme props

--- ui/theme/menu.lua
+++ ui/theme/menu.lua
@@ -10,9 +10,18 @@ local dpi = xresources.apply_dpi
 
 
 local _M = {
-  menu_submenu_icon = "󰁖",  -- nf-md-arrow_right_bold_circle
+  -- Properties:
+  menu_font = "",
+  menu_submenu = " ",  -- nf-cod-triangle_right
   menu_height = dpi(30),
   menu_width  = dpi(150),
+  menu_border_width = dpi(1), 	-- The menu item border width.
+  --Colors:
+  menu_border_color = color_scheme.base00, 	-- The menu item border color.
+  menu_fg_focus = color_scheme.base0D, 	    -- The default focused item foreground (text) color.
+  menu_bg_focus = color_scheme.base00, 	    -- The default focused item background color.
+  menu_fg_normal = color_scheme.base04, 	    -- The default foreground (text) color.
+  menu_bg_normal = color_scheme.base00 	    -- The default background color.
 }
 
 -- Generate Awesome icon:
