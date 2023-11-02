3594c88429516fffea15caa9c542931f5cf35cda
MOIS3Y <stepan@zhukovsky.me>
2023-11-02 15:49:25 +0900

    Add: rc.lua entrypoint description

--- rc.lua
+++ rc.lua
@@ -1,8 +1,23 @@
+-- ▄▀█ █░█░█   █▀▀ █░█ ▄▀█ █▀▄ ▀
+-- █▀█ ▀▄▀▄▀   █▄▄ █▀█ █▀█ █▄▀ ▄
+-- -- -- -- -- -- -- -- -- -- --
+
+--[[
+  Entrypoint to run the AWchad configuration:
+  STANDALONE: awesome -c ./rc.lu
+  SDDM: awesome -c $HOME/.config/awesome/rc.lua
+--]]
+
 pcall(require, "luarocks.loader")
 
+-- When loaded, this module makes sure that there's always a client
+-- that will have focus on events 
+-- such as tag switching, client unmanaging, etc:
 require "awful.autofocus"
 
+-- Realtime error handler:
 require "core.error_handler"
 
+-- Configuration pkgs:
 require "ui"
 require "core"
