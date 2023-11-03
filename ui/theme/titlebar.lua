7ee18df2f007593b4dcbcb962970273741b2a54b
MOIS3Y <stepan@zhukovsky.me>
2023-11-03 13:09:24 +0900

    Modify: titlebar icons follow theme color

--- ui/theme/titlebar.lua
+++ ui/theme/titlebar.lua
@@ -2,33 +2,74 @@
 -- ░█░ █ ░█░ █▄▄ ██▄ █▄█ █▀█ █▀▄ ▄
 -- -- -- -- -- -- -- -- -- -- -- -
 
+-- Imports:
+local gears = require "gears"
+local gfs   = require "gears.filesystem"
+
 local font = require "ui.theme.font"
 local path = require "ui.theme.path"
 
-local icons_path = path.icons .. "titlebar/"
+-- Import current color scheme:
+local colors = require "ui.theme.color_scheme"
 
+-- Set btn colors:
+local normal   = colors.base02
+local close    = colors.base08
+local minimize = colors.base0A
+local maximize = colors.base0B
 
+-- Recolor images func shourtcut: 
+local gcrci = gears.color.recolor_image
+
+-- Icon path shourtcut:
+local ip = path.icons .. "titlebar/"
+
+-- Recolor buttons:
+local btn = {
+  -- Close Button icons:
+  close_normal       = gcrci(ip .. "normal.svg", normal),
+  close_focus        = gcrci(ip .. "close_focus.svg", close),
+  close_normal_hover = gcrci(ip .. "close_focus_hover.svg", close),
+  close_focus_hover  = gcrci(ip .. "close_focus_hover.svg", close),
+  -- Minimize Button icons:
+  min_normal         = gcrci(ip .. "normal.svg", normal),
+  min_focus          = gcrci(ip .. "minimize_focus.svg", minimize),
+  min_normal_hover   = gcrci(ip .. "minimize_focus_hover.svg", minimize),
+  min_focus_hover    = gcrci(ip .. "minimize_focus_hover.svg", minimize),
+  -- Maximized Button (While Window is Maximized) icons:
+  max_normal_active       = gcrci(ip .. "normal.svg", normal),
+  max_focus_active        = gcrci(ip .. "maximized_focus.svg", maximize),
+  max_normal_active_hover = gcrci(ip .. "maximized_focus_hover.svg", maximize),
+  max_focus_active_hover  = gcrci(ip .. "maximized_focus_hover.svg", maximize),
+  -- Maximized Button (While Window is not Maximized) icons:
+  max_normal_inactive       = gcrci(ip .. "normal.svg", normal),
+  max_focus_inactive        = gcrci(ip .. "maximized_focus.svg", maximize),
+  max_normal_inactive_hover = gcrci(ip .. "maximized_focus_hover.svg", maximize),
+  max_focus_inactive_hover  = gcrci(ip .. "maximized_focus_hover.svg", maximize)
+}
+
+-- Register buttons:
 return {
   -- Titlebar client name font:
   font_title = font.titlebar,
   -- Close Button icons:
-  titlebar_close_button_normal = icons_path  .. "normal.svg",
-  titlebar_close_button_focus = icons_path  .. "close_focus.svg",
-  titlebar_close_button_normal_hover = icons_path  .. "close_focus_hover.svg",
-  titlebar_close_button_focus_hover = icons_path  .. "close_focus_hover.svg",
+  titlebar_close_button_normal          = btn.close_normal,
+  titlebar_close_button_focus           = btn.close_focus,
+  titlebar_close_button_normal_hover    = btn.close_normal_hover,
+  titlebar_close_button_focus_hover     = btn.close_focus_hover,
   -- Minimize Button icons:
-  titlebar_minimize_button_normal = icons_path  .. "normal.svg",
-  titlebar_minimize_button_focus = icons_path  .. "minimize_focus.svg",
-  titlebar_minimize_button_normal_hover = icons_path  .. "minimize_focus_hover.svg",
-  titlebar_minimize_button_focus_hover = icons_path  .. "minimize_focus_hover.svg",
+  titlebar_minimize_button_normal       = btn.min_normal,
+  titlebar_minimize_button_focus        = btn.min_focus,
+  titlebar_minimize_button_normal_hover = btn.min_normal_hover,
+  titlebar_minimize_button_focus_hover  = btn.min_focus_hover,
   -- Maximized Button (While Window is Maximized) icons:
-  titlebar_maximized_button_normal_active = icons_path  .. "normal.svg",
-  titlebar_maximized_button_focus_active = icons_path  .. "maximized_focus.svg",
-  titlebar_maximized_button_normal_active_hover = icons_path  .. "maximized_focus_hover.svg",
-  titlebar_maximized_button_focus_active_hover = icons_path  .. "maximized_focus_hover.svg",
+  titlebar_maximized_button_normal_active       = btn.max_normal_active,
+  titlebar_maximized_button_focus_active        = btn.max_focus_active,
+  titlebar_maximized_button_normal_active_hover = btn.max_normal_active_hover,
+  titlebar_maximized_button_focus_active_hover  = btn.max_focus_active_hover,
   -- Maximized Button (While Window is not Maximized) icons:
-  titlebar_maximized_button_normal_inactive = icons_path  .. "normal.svg",
-  titlebar_maximized_button_focus_inactive = icons_path  .. "maximized_focus.svg",
-  titlebar_maximized_button_normal_inactive_hover = icons_path  .. "maximized_focus_hover.svg",
-  titlebar_maximized_button_focus_inactive_hover = icons_path  .. "maximized_focus_hover.svg"
+  titlebar_maximized_button_normal_inactive       = btn.max_normal_inactive,
+  titlebar_maximized_button_focus_inactive        = btn.max_focus_inactive,
+  titlebar_maximized_button_normal_inactive_hover = btn.max_normal_inactive_hover,
+  titlebar_maximized_button_focus_inactive_hover  = btn.max_focus_inactive_hover
 }
